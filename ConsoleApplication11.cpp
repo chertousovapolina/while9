c61cc07ad0c9ac2849addb18433c48248ecab894
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-17 18:44:36 +0300

    Add files via upload

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,18 @@
+﻿// дз чертоусова while9;
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int n, k = 1, a = 3;
+	cin >> n;
+	while(a<=n){
+		a *= 3;
+		k++;
+	}
+	cout << k;
+
+
+
+}
+
